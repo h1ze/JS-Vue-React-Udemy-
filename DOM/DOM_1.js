bae3c34523666b1a55a8cd9f0adc74189151ef49
h1ze <burgov@mail.ru>
2022-07-07 22:03:03 +0400

    update DOM_1.js

--- DOM/DOM_1.js
+++ DOM/DOM_1.js
@@ -1,1 +1,24 @@
-console.log(document.documentElement.nextElementSibling);
+// Зная структуру html, с помощью изученных
+// методов получить (в консоль):
+// 1. head
+// 2. body
+// 3. все дочерние элементы body и вывести их в
+// консоль.
+// 4. первый div и все его дочерние узлы
+// а) вывести все дочерние узлы в консоль
+// б) вывести в консоль все дочерние узлы,
+// кроме первого и последнего
+// Для навигации по DOM использовать методы,
+// которые возвращают только элементы
+
+console.log(document.head);
+console.log(document.body);
+console.log(document.body.children);
+console.log(document.body.firstElementChild);
+console.log(
+  document.body.firstElementChild.firstElementChild.nextElementSibling
+);
+console.log(
+  document.body.firstElementChild.firstElementChild.nextElementSibling
+    .nextElementSibling
+);
