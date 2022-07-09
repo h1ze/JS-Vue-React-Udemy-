114b668b82992854189e8d14fd740b020f422454
h1ze <burgov@mail.ru>
2022-07-09 16:55:36 +0400

    update DOM_2.js

--- DOM/DOM_2.js
+++ DOM/DOM_2.js
@@ -32,3 +32,28 @@ console.log(
 
 console.log(document.querySelector("ul"), document.querySelector("mark"));
 console.log(document.body.children[0]);
+
+// 2. Получить список всех ссылок, которые не находятся внутри списка ul
+
+console.log(document.querySelectorAll("a"));
+let hrefNodeList = document.querySelectorAll("a");
+let arrHrefNodeList = Array.from(hrefNodeList);
+// let hrefHtmlCollection = document.getElementsByTagName("a");
+// console.log(arrHrefNodeList);
+// console.log(typeof hrefNodeList, typeof arrHrefNodeList);
+// console.log(hrefHtmlCollection);
+// console.log(typeof hrefHtmlCollection);
+// let arrHrefHtmlCollection = [...hrefHtmlCollection];
+// console.log(arrHrefHtmlCollection);
+let resultHrefList = arrHrefNodeList.filter((el) => {
+  if (el.parentElement.nodeName !== "LI") {
+    return el;
+  }
+});
+console.log(resultHrefList);
+
+// 3. Найти элемент, который находится перед и после списка ul
+let ulList = document.querySelector("ul");
+let previousEl = ulList.previousElementSibling;
+let nextEl = ulList.nextElementSibling;
+console.log(ulList, previousEl, nextEl);
